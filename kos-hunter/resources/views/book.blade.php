c1a4f0ee12b7042ed4f6e35193ccc1798a36de83
Sayyed Aamir <mehmetvladimir@gmail.com>
2024-06-27 11:35:56 +0700

    book

--- kos-hunter/resources/views/book.blade.php
+++ kos-hunter/resources/views/book.blade.php
@@ -3,7 +3,7 @@
     <x-slot name="header">
         <h2
             class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-            {{ $properti->tipe }} {{  $properti->nama  }}
+             {{  $properti->nama  }}
         </h2>
 
         <h2
